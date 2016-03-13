41cdcaaa5915ebd94e7692653537e865d132fc7e
jeffersonhwang <jefferson.hwang@gmail.com>
2016-03-13 16:23:26 -0700

    Added persistence of reading mode
    Reading mode persists when refreshing and going to a new page.

--- reading-mode.js
+++ reading-mode.js
@@ -1,14 +1,18 @@
 /*
  * Content script injected onto page
  */
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		toggleReadingMode(request.toggle);
+	}
+);
 
-function toggleReadingMode(on)
-{
+function toggleReadingMode(inReadingMode) {
 	var primary = document.getElementById("primary");
 	var secondary = document.getElementById("secondary");
 	var article = document.getElementsByTagName("article")[0];
 
-	if (on) {
+	if (inReadingMode) {
 		primary.classList.add("maximize");
 		article.classList.add("maximize");
 		secondary.classList.add("minimize");
@@ -18,10 +22,21 @@ function toggleReadingMode(on)
 		article.classList.remove("maximize");
 		secondary.classList.remove("minimize");
 	}
+
+	chrome.storage.local.set({"readingMode" : inReadingMode}, function(){
+		console.log("Reading mode: " + inReadingMode);
+	});
 }
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		toggleReadingMode(request.toggle);
-	}
-);
+// persists reading mode if turned on when reloading/going to a new page
+function window_OnLoad()
+{
+	// look it up from local storage
+	// if true, toggle it on
+	chrome.storage.local.get("readingMode", function(items){
+		if(items.readingMode)
+			toggleReadingMode(items.readingMode);
+	});
+}
+
+window.onload = window_OnLoad;
